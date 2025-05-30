433c233cc26f487ab24384fc9e0900f71bc1eb22
Gersomsim <gersomhernandez@me.com>
2025-05-29 20:54:47 -0600

    feat: agregar modelo Event con relaciones y soft deletes

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,5 +7,64 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'event_start_date',
+        'event_end_date',
+        'status',
+        'available_places',
+        'category_id',
+        'location_id',
+    ];
+    protected $casts = [
+        'event_start_date' => 'datetime',
+        'event_end_date' => 'datetime',
+    ];
+
+    public function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->slug = Str::slug($model->name);
+        });
+    }
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function organizers()
+    {
+        return $this->belongsToMany(Organizer::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(EventTask::class);
+    }
+
+    public function registrations()
+    {
+        return $this->hasMany(EventRegistration::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(EventReview::class);
+    }
+
+    public function recommendations()
+    {
+        return $this->hasMany(EventRecommendation::class);
+    }
+    
 }

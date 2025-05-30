1014d2630d1f86ee334429e3674a4ad16a8c0925
Gersomsim <gersomhernandez@me.com>
2025-05-29 21:02:20 -0600

    feat: agregar relaciones y soft deletes en EventReview

--- app/Models/EventReview.php
+++ app/Models/EventReview.php
@@ -7,5 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class EventReview extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'rating',
+        'comment',
+        'event_id',
+        'profile_id',
+    ];
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    public function profile()
+    {
+        return $this->belongsTo(Profile::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(ReviewComment::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(ReviewLike::class);
+    }
 }
